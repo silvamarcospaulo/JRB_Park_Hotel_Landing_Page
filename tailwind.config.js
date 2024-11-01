4ffb7a02001dac28e83f233ba92ce28fc5a86104
Marcos Paulo <smpmarcospaulo@icloud.com>
2024-11-01 14:53:18 -0300

    Nono  commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html, js}",
+    "./src/**/*.{html,js,svelte,ts}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
